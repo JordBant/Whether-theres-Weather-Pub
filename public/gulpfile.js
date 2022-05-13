432e741a9e21ea6b07b6d394fe65d355dcfc0465
JordBant <jaybant23@gmail.com>
2022-05-13 13:22:41 -0400

    Gulp watchDev function synced

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,23 +1,24 @@
-const gulp = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify')
 
 const buildStyles = () => {
-  return gulp.src('./styles/**/*.scss')
+  return src('./styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(dest('./dist/css'));
 };
 
 const uglifyJS = () => {
-    return gulp.src('./*.js')
+    return gulp.src('./script.js')
     .pipe(uglify())
-    .pipe(gulp.dest('./dist/bundleJS'));
-
+    .pipe(dest('./dist/bundleJS'));
 }
 
 const watchDev = () => {
-    
+    watch('./script.js', uglifyJS)
+    watch('./styles/*.scss',buildStyles)
 }
 
 exports.buildStyles = buildStyles;
 exports.uglifyJS = uglifyJS;
+exports.watchDev = watchDev
