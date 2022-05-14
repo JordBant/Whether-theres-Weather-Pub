d980be7fffafd9edda35ac9a3177fdeac4fe6423
JordBant <jaybant23@gmail.com>
2022-05-14 07:46:40 -0400

    Updated gulpfile

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,12 +1,19 @@
 const { src, dest, watch } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css')
 
 const buildStyles = () => {
   return src('./styles/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(dest('./dist/css'));
 };
+
+const uglifyCSS = () => {
+    return src('./dist/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(dest('dist/css'))
+}
 
 const uglifyJS = () => {
     return gulp.src('./script.js')
@@ -15,10 +22,11 @@ const uglifyJS = () => {
 }
 
 const watchDev = () => {
-    watch('./script.js', uglifyJS)
     watch('./styles/*.scss',buildStyles)
+    watch('./script.js', uglifyJS)
 }
 
 exports.buildStyles = buildStyles;
 exports.uglifyJS = uglifyJS;
+exports.uglifyCSS = uglifyCSS
 exports.watchDev = watchDev
